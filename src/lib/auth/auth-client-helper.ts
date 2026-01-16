e76a65fe9ad712ad3d493f1408578b18b0c29d1c
Wilson Angelie Tan <wilsonangelietann@gmail.com>
2026-01-16 10:19:12 +0700

    fix: rewrite auth-client-helper function

--- src/lib/auth/auth-client-helper.ts
+++ src/lib/auth/auth-client-helper.ts
@@ -1,10 +1,17 @@
 "use client";
-import { useSession } from "./auth-client";
 
-export function requireUser() {
+import { useSession } from "./auth-client";
+import { useRouter } from "next/navigation";
+
+// NOTE: This can be used for client-side components that need to ensure the user is logged in.
+export function useRequireUser() {
   const session = useSession();
+  const router = useRouter();
+
   if (!session.data) {
-    throw new Error("User is not logged in");
+    router.replace("/login");
+    return { session: null, loading: false };
   }
+  
   return session;
 }
